1ddee983cc6ab8135ae1d818ada91365d05847bb
Justin Toniazzo <jt@thoughtbot.com>
2025-05-09 10:20:36 -0400

    wip

--- lua/neotest-bun/init.lua
+++ lua/neotest-bun/init.lua
@@ -1,6 +1,10 @@
-local main = require("neotest-bun.main")
-local config = require("neotest-bun.config")
+local main = require("neotest-bun/main")
+local async = require("neotest/async")
+local config = require("neotest-bun/config")
+local bun = require("neotest-bun/util/bun")
 local lib = require("neotest/lib")
+local neotestFileUtil = require("neotest/lib/file")
+local logger = require("neotest.logging")
 
 local NeotestBun = {
   name = "neotest-bun",
@@ -36,6 +40,212 @@ end
 
 NeotestBun.root = function(path)
   return lib.files.match_root_pattern("bun.lock")(path)
+end
+
+NeotestBun.filter_dir = function(name)
+  return name ~= "node_modules"
+end
+
+NeotestBun.is_test_file = function(file_path)
+  if file_path == nil then
+    return false
+  end
+
+  if string.find(file_path, ".test.tsx") then
+    return true
+  else
+    return false
+  end
+end
+
+NeotestBun.discover_positions = function(path)
+  local query = [[
+    ; -- Namespaces --
+    ; Matches: `describe('context', () => {})`
+    ((call_expression
+      function: (identifier) @func_name (#eq? @func_name "describe")
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
+    ; Matches: `describe('context', function() {})`
+    ((call_expression
+      function: (identifier) @func_name (#eq? @func_name "describe")
+      arguments: (arguments (string (string_fragment) @namespace.name) (function_expression))
+    )) @namespace.definition
+    ; Matches: `describe.only('context', () => {})`
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "describe")
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
+    ; Matches: `describe.only('context', function() {})`
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "describe")
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (function_expression))
+    )) @namespace.definition
+    ; Matches: `describe.each(['data'])('context', () => {})`
+    ((call_expression
+      function: (call_expression
+        function: (member_expression
+          object: (identifier) @func_name (#any-of? @func_name "describe")
+        )
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
+    ; Matches: `describe.each(['data'])('context', function() {})`
+    ((call_expression
+      function: (call_expression
+        function: (member_expression
+          object: (identifier) @func_name (#any-of? @func_name "describe")
+        )
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (function_expression))
+    )) @namespace.definition
+
+    ; -- Tests --
+    ; Matches: `test('test') / it('test')`
+    ((call_expression
+      function: (identifier) @func_name (#any-of? @func_name "it" "test")
+      arguments: (arguments (string (string_fragment) @test.name) [(arrow_function) (function_expression)])
+    )) @test.definition
+    ; Matches: `test.only('test') / it.only('test')`
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "test" "it")
+      )
+      arguments: (arguments (string (string_fragment) @test.name) [(arrow_function) (function_expression)])
+    )) @test.definition
+    ; Matches: `test.each(['data'])('test') / it.each(['data'])('test')`
+    ((call_expression
+      function: (call_expression
+        function: (member_expression
+          object: (identifier) @func_name (#any-of? @func_name "it" "test")
+          property: (property_identifier) @each_property (#eq? @each_property "each")
+        )
+      )
+      arguments: (arguments (string (string_fragment) @test.name) [(arrow_function) (function_expression)])
+    )) @test.definition
+  ]]
+
+  local positions = lib.treesitter.parse_positions(path, query, {
+    nested_tests = false,
+    build_position = 'require("neotest-bun").build_position',
+  })
+
+  return positions
+end
+
+local function get_match_type(captured_nodes)
+  if captured_nodes["test.name"] then
+    return "test"
+  end
+  if captured_nodes["namespace.name"] then
+    return "namespace"
+  end
+end
+
+function NeotestBun.build_position(file_path, source, captured_nodes)
+  local match_type = get_match_type(captured_nodes)
+  if not match_type then
+    return
+  end
+
+  ---@type string
+  local name = vim.treesitter.get_node_text(captured_nodes[match_type .. ".name"], source)
+  local definition = captured_nodes[match_type .. ".definition"]
+
+  return {
+    type = match_type,
+    path = file_path,
+    name = name,
+    range = { definition:range() },
+    is_parameterized = captured_nodes["each_property"] and true or false,
+  }
+end
+
+NeotestBun.build_spec = function(args)
+  local results_path = async.fn.tempname() .. ".xml"
+  local tree = args.tree
+
+  if not tree then
+    return
+  end
+
+  local pos = args.tree:data()
+  local testNamePattern = "'.*'"
+
+  if pos.type == "test" or pos.type == "namespace" then
+    -- pos.id in form "path/to/file::Describe text::test text"
+    local testName = string.sub(pos.id, string.find(pos.id, "::") + 2)
+    testName, _ = string.gsub(testName, "::", " ")
+    testNamePattern = bun.escapeTestPattern(testName)
+    testNamePattern = "'^" .. testNamePattern
+    if pos.type == "test" then
+      testNamePattern = testNamePattern .. "$'"
+    else
+      testNamePattern = testNamePattern .. "'"
+    end
+  end
+
+  local binary = "bun test"
+  local command = vim.split(binary, "%s+")
+
+  vim.list_extend(command, {
+    "--reporter=junit",
+    "--reporter-outfile=" .. results_path,
+    bun.escapeTestPattern(vim.fs.normalize(pos.path)),
+  })
+
+  -- creating empty file for streaming results
+  lib.files.write(results_path, "")
+  local stream_data, stop_stream = neotestFileUtil.stream(results_path)
+
+  return {
+    command = command,
+    context = {
+      results_path = results_path,
+      file = pos.path,
+      stop_stream = stop_stream,
+    },
+    stream = function()
+      return function()
+        local new_results = stream_data()
+        local ok, parsed = pcall(vim.json.decode, new_results, { luanil = { object = true } })
+
+        if not ok or not parsed.testResults then
+          return {}
+        end
+
+        return bun.parsedJsonToResults(parsed, results_path, nil)
+      end
+    end,
+  }
+end
+
+function NeotestBun.results(spec, b, tree)
+  spec.context.stop_stream()
+
+  local output_file = spec.context.results_path
+
+  local success, data = pcall(lib.files.read, output_file)
+
+  if not success then
+    logger.error("No test output file found ", output_file)
+    return {}
+  end
+
+  local ok, parsed = pcall(vim.xml.decode, data, { luanil = { object = true } })
+
+  if not ok then
+    logger.error("Failed to parse test output json ", output_file)
+    return {}
+  end
+
+  local results = bun.parsedJsonToResults(parsed, output_file, b.output)
+
+  return results
 end
 
 _G.NeotestBun = NeotestBun
--- lua/neotest-bun/util/bun.lua
+++ lua/neotest-bun/util/bun.lua
@@ -15,4 +15,78 @@ function bun.isBunProject()
   return bun.fileExists(rootBunLock)
 end
 
+function bun.escapeTestPattern(s)
+  return s
+  -- return (
+  --   s:gsub("%(", "%\\(")
+  --     :gsub("%)", "%\\)")
+  --     :gsub("%]", "%\\]")
+  --     :gsub("%[", "%\\[")
+  --     :gsub("%*", "%\\*")
+  --     :gsub("%+", "%\\+")
+  --     :gsub("%-", "%\\-")
+  --     :gsub("%?", "%\\?")
+  --     :gsub("%$", "%\\$")
+  --     :gsub("%^", "%\\^")
+  --     :gsub("%/", "%\\/")
+  --     :gsub("%'", "%\\'")
+  -- )
+end
+
+function bun.parsedJsonToResults(data, output_file, consoleOut)
+  local tests = {}
+
+  -- for _, testResult in pairs(data.testResults) do
+  --   local testFn = testResult.name
+  --   for _, assertionResult in pairs(testResult.assertionResults) do
+  --     local status, name = assertionResult.status, assertionResult.title
+  --
+  --     if name == nil then
+  --       logger.error("Failed to find parsed test result ", assertionResult)
+  --       return {}
+  --     end
+  --
+  --     local keyid = testFn
+  --
+  --     for _, value in ipairs(assertionResult.ancestorTitles) do
+  --       keyid = keyid .. "::" .. value
+  --     end
+  --
+  --     keyid = keyid .. "::" .. name
+  --
+  --     if status == "pending" then
+  --       status = "skipped"
+  --     end
+  --
+  --     tests[keyid] = {
+  --       status = status,
+  --       short = name .. ": " .. status,
+  --       output = consoleOut,
+  --       location = assertionResult.location,
+  --     }
+  --
+  --     if not vim.tbl_isempty(assertionResult.failureMessages) then
+  --       local errors = {}
+  --
+  --       for i, failMessage in ipairs(assertionResult.failureMessages) do
+  --         local msg = cleanAnsi(failMessage)
+  --         local errorLine, errorColumn = findErrorPosition(testFn, msg)
+  --
+  --         errors[i] = {
+  --           line = (errorLine or assertionResult.location.line) - 1,
+  --           column = (errorColumn or 1) - 1,
+  --           message = msg,
+  --         }
+  --
+  --         tests[keyid].short = tests[keyid].short .. "\n" .. msg
+  --       end
+  --
+  --       tests[keyid].errors = errors
+  --     end
+  --   end
+  -- end
+
+  return tests
+end
+
 return bun
