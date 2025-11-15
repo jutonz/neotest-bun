589e28606529508871ea449f07b5be85a942fed8
Justin Toniazzo <jt@thoughtbot.com>
2025-11-15 15:44:00 -0500

    Add script for autogenerating junit output
    I'd been manually adding it, but this will make it easier to test
    against real tests

---
+++ tests/fixtures/bun_tests/tests/one-passed.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "bun:test";
+
+test("it works", () => {
+  expect(true).toBe(true);
+});
--- tests/fixtures/bun_tests/tests/simple.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from "bun:test";
-
-describe("something", () => {
-  test("2 + 2", () => {
-    expect(2 + 2).toBe(4);
-  });
-})
